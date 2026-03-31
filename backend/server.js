42088107e974214935693385d3660934c562dfe0
trantrongha-eiu <ha.trantrong.cit21@eiu.edu.vn>
2026-03-31 22:31:07 +0700

    fix api url

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+// Cloudinary config
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:    process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 app.use(cors());
@@ -20,8 +27,8 @@ app.get('/', (req, res) => {
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/vocab', require('./routes/vocab'));
 app.use('/api/vocabbook', require('./routes/vocabBook'));
-app.use('/api/reading', require('./routes/reading'));   // ← file mới
-app.use('/api/admin', require('./routes/admin'));      // ← mới
+app.use('/api/reading', require('./routes/reading')); 
+app.use('/api/admin', require('./routes/admin'));      
 app.use('/api/listening', require('./routes/listening'));
 app.use('/api/writing', require('./routes/writing'));
 app.use('/api/history', require('./routes/history'));
